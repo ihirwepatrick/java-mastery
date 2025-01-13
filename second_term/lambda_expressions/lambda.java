50d27fcb3536fb4b2ce759d6ee1a0ff3e7489850
unknown <nipcts3@gmail.com>
2025-01-13 11:10:48 +0200

    used lambda

--- second_term/lambda_expressions/lambda.java
+++ second_term/lambda_expressions/lambda.java
@@ -1,2 +1,15 @@
-public class lambda {
+// Functional Interface
+@FunctionalInterface
+interface Greeting {
+    void sayHello(String name);
 }
+
+public class lambda {
+    public static void main(String[] args) {
+        // Using Lambda expression to implememt Greeting interface
+        Greeting greeting = (name) -> System.out.println("Hello, " + name + "!");
+        greeting.sayHello("Patrick");
+    }
+}
+
+// here we are using a lambda expression to implement an interface without having to create another class
