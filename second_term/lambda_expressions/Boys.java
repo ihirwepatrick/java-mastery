90970889bad1c01dc7c32e0af45c9d9dae0d0b11
unknown <nipcts3@gmail.com>
2025-01-13 11:56:30 +0200

    used Thread.sleep to optimize the output

--- second_term/lambda_expressions/Boys.java
+++ second_term/lambda_expressions/Boys.java
@@ -2,6 +2,11 @@ public class Boys extends Thread{
     public void run () {
         for (int i =0; i <100; i++) {
             System.out.println("Boy");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
--- second_term/lambda_expressions/Girls.java
+++ second_term/lambda_expressions/Girls.java
@@ -2,6 +2,11 @@ public class Girls extends Thread{
     public void run () {
         for (int i =0; i <100; i++) {
             System.out.println("Girl");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
