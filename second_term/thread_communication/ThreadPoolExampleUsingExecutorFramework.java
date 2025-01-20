51890736b6fa6ea27ed353291d3abc84a8ef18d3
unknown <nipcts3@gmail.com>
2025-01-20 11:57:59 +0200

    used fixed thread pool

--- second_term/thread_communication/ThreadPoolExampleUsingExecutorFramework.java
+++ second_term/thread_communication/ThreadPoolExampleUsingExecutorFramework.java
@@ -4,42 +4,59 @@ import java.util.concurrent.Executors;
 class BoyA implements Runnable {
     public void run() {
         System.out.println("Boy Thread started");
-        for (int i = 1; i < 100; i++) {
-            System.out.println("Boy: " + i);
+        for (int i = 1; i < 20; i++) {
+            System.out.print("Boy: " + i + " ");
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
         System.out.println("Boy Thread ended");
     }
 }
+
 class GirlA implements Runnable {
     public void run() {
         System.out.println("Girl Thread started");
-        for (int i = 1; i < 100; i++) {
-            System.out.println("Girl: " + i);
+        for (int i = 1; i < 20; i++) {
+            System.out.print("Girl: " + i + " ");
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
         System.out.println("Girl Thread ended");
     }
 }
 
-
-
-
+class TeacherA implements Runnable {
+    public void run() {
+        System.out.println("Teacher Thread started");
+        for (int i = 1; i < 20; i++) {
+            System.out.print("Teacher: " + i + " ");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Teacher Thread ended");
+    }
+}
 
 public class ThreadPoolExampleUsingExecutorFramework {
     public static void main(String[] args) {
-        ExecutorService executir = Executors.newSingleThreadExecutor();
-        executir.execute(new BoyA());
-        executir.execute(new GirlA());
+        // Create a fixed thread pool with 2 threads
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        // Submit tasks to the pool
+        executor.execute(new BoyA());
+        executor.execute(new GirlA());
+        executor.execute(new TeacherA());
+
+        // Shutdown the executor after tasks are completed
+        executor.shutdown();
     }
 }
