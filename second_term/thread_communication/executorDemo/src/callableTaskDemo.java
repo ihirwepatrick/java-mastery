959255e6740e50f2c7fee5826bb21eaf1c53be37
unknown <nipcts3@gmail.com>
2025-01-21 09:27:50 +0200

    added more messages

--- second_term/thread_communication/executorDemo/src/callableTaskDemo.java
+++ second_term/thread_communication/executorDemo/src/callableTaskDemo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 class callableTaskA implements Callable<String> {
@@ -13,10 +15,20 @@ class callableTaskA implements Callable<String> {
 public class callableTaskDemo {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(5);
+        List<callableTaskA> tasks = new ArrayList<callableTaskA>();
+        tasks.add(new callableTaskA("Hello World"));
+        tasks.add(new callableTaskA("Hello Year 2"));
+        tasks.add(new callableTaskA("Hello Year 3"));
+        tasks.add(new callableTaskA("Hello Year 1"));
+        List<Future<String>> results = executor.invokeAll(tasks);
+        for (Future<String> result : results) {
+            System.out.println(result.get());
+        }
         // when u submit a callable task we access it into a future
         // (like a promise that we can get a value from there)
         Future<String> future = executor.submit(new callableTaskA("Hello World"));
         String message = future.get();
+
         System.out.println("Message:" + message);
         executor.shutdown();
         System.out.println("Done");
