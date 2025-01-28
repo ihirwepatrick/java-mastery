d2267e6abf5e2678055736f2ff30ead389cd986e
unknown <nipcts3@gmail.com>
2025-01-28 09:05:57 +0200

    added res

--- second_term/DynamicWebApplication/src/main/java/org/example/dynamicwebapplication/sumServlet.java
+++ second_term/DynamicWebApplication/src/main/java/org/example/dynamicwebapplication/sumServlet.java
@@ -4,9 +4,16 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class sumServlet extends HttpServlet {
-    public void service(HttpServletRequest request, HttpServletResponse response) {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class sumServlet extends HttpServlet {
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+int num1 = Integer.parseInt(request.getParameter("num1"));
+int num2 = Integer.parseInt(request.getParameter("num2"));
+int sum = num1 + num2;
+PrintWriter out = response.getWriter();
+out.println(sum);
     }
 
 }
